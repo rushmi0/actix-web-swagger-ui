26b5579d90a9e3747344d18bf9fa508067857843
lnwza007 ⚡️ <gcalku@gmail.com>
2024-12-13 16:09:46 +0700

    update

--- src/main/services/api/v1/hello.rs
+++ src/main/services/api/v1/hello.rs
@@ -1,14 +1,33 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use actix_web::{get, web, HttpResponse, Responder};
 use serde::Serialize;
+use utoipa::{ToSchema, OpenApi};
+
 use crate::module::greet;
 
-#[derive(Serialize)]
-struct Greeting {
+#[derive(Serialize, ToSchema)]
+pub struct Greeting {
+    /// Unix timestamp when the greeting was created
     timestamp: u32,
+    /// Greeting message
     message: String,
 }
 
+/// Greeting service
+///
+/// Returns a personalized greeting message along with the current timestamp.
+#[utoipa::path(
+    get,
+    path = "/api/v1/{name}",
+    params(
+        ("name" = String, Path, description = "Name of the person to greet. This is a required path parameter.")
+    ),
+    responses(
+        (status = 200, description = "A successful response containing a greeting message and timestamp.", body = Greeting),
+        (status = 400, description = "Bad request, typically when no name is provided or invalid input."),
+        (status = 500, description = "Internal server error, for unexpected issues.")
+    )
+)]
 #[get("/{name}")]
 pub async fn hello_service(name: web::Path<String>) -> impl Responder {
     let greeting_message: String = greet(&name).await;
--- src/main/services/api/v1/mod.rs
+++ src/main/services/api/v1/mod.rs
@@ -1,15 +1,34 @@
-use actix_web::web;
-
 mod index;
 mod hello;
 
 use index::index_service;
 use hello::hello_service;
 
+use actix_web::web;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(hello::hello_service),
+    components(schemas(hello::Greeting)),
+    info(
+        title = "My API อ่านหาพ่องงง มึงเหรอ",
+        version = "0.1.0",
+        description = "his is a sample API that demonstrates functionality for greeting users."
+    )
+)]
+struct ApiDoc;
+
 pub fn service_hub(cfg: &mut web::ServiceConfig) {
+    let api_doc = ApiDoc::openapi();
     cfg.service(
         web::scope("/api/v1")
             .service(index_service)
             .service(hello_service)
+    );
+    cfg.service(
+        SwaggerUi::new("/doc/v1/{_:.*}")
+            .url("/doc-api-v1/openapi.json", api_doc.clone())
     );
 }
